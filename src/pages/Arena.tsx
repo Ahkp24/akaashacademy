f4d0757a64afd4662179003b499284018a1cd468
Ahkp24 <akaashkpatel@icloud.com>
2026-06-14 11:13:07 -0400

    Delete src/pages/Arena.tsx

--- src/pages/Arena.tsx
+++
@@ -1,8 +0,0 @@
-// TODO: Akaash Arena page content
-export default function Arena() {
-  return (
-    <main className="min-h-screen flex items-center justify-center">
-      <span className="font-heading text-6xl text-white/10">ARENA</span>
-    </main>
-  );
-}
