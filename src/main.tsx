3628295b1f0863828552b81c3963c3ad403deb9c
Ahkp24 <akaashkpatel@icloud.com>
2026-06-29 14:32:44 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// 1. We have to import the router here!
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    {/* 2. Wrap your entire app inside the router */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
