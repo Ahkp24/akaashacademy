e16c0e5c95b12fe13bfadaca83c7111fbe770e40
Ahkp24 <akaashkpatel@icloud.com>
2026-06-29 14:15:38 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import SAT from "./pages/SAT";
 import ACT from "./pages/ACT";
-import About from "./pages/About";
 import Tips from "./pages/Tips";
+import About from "./pages/About";
+import Footer from "./components/Footer";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
-      <div className="bg-black min-h-screen flex flex-col">
-        <Navbar />
-        <div className="flex-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/reading" element={<SAT />} />
-            <Route path="/math" element={<ACT />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/tips" element={<Tips />} />
-          </Routes>
-        </div>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div className="flex flex-col min-h-screen bg-black text-white selection:bg-[#FF0000] selection:text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/reading" element={<SAT />} />
+        <Route path="/math" element={<ACT />} />
+        <Route path="/tips" element={<Tips />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 }
+
+export default App;
